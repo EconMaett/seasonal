c3d3ba253b839242147410ccefd39fa25d1acf33
EconMaett <matthias.spichiger@bluewin.ch>
2024-02-01 18:18:40 +0100

    Initial commit

---
+++ R/01_introduction.R
@@ -0,0 +1,36 @@
+# 01 seasonal - Introduction ----
+
+# The R package is available on:
+# - CRAN: https://cran.r-project.org/package=seasonal
+# - GitHub: https://github.com/christophsax/seasonal
+# - Website: http://www.seasonal.website/
+
+# Vignette: Adjusting multiple series
+# https://cran.r-project.org/web/packages/seasonal/vignettes/multiple.html
+
+
+## Setup ----
+
+# The R package is on CRAN
+# install.packages("seasonal")
+library(seasonal)
+
+citation("seasonal")
+
+
+## Basics ----
+
+
+## Specifying the specs ----
+
+
+## Specifying multiple series ----
+
+
+## Backend ----
+
+
+## Composite spec ----
+
+
+# END
